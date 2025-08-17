1a31d518e9acb6aeae236dfd5320bbabd89c2f85
sama5677 <sm4786400@gmail.com>
2025-08-18 02:03:46 +0300

    home

--- Frontend/src/app/Components/home/home.component.ts
+++ Frontend/src/app/Components/home/home.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // لازم عشان *ngFor و | currency
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  products = [
+    { id: 1, name: 'Laptop Gold', description: 'This is a Dell laptop', price: 600, image: 'assets/laptop.jpg' },
+    { id: 2, name: 'Phone', description: 'This is a phone', price: 6000, image: 'assets/phone.jpg' },
+    { id: 3, name: 'Headphones', description: 'Noise cancelling headphones', price: 1200, image: 'assets/headphones.jpg' },
+    { id: 4, name: 'Smart Watch', description: 'Track your fitness', price: 2000, image: 'assets/watch.jpg' }
+  ];
 }
+
--- Frontend/src/app/app.config.ts
+++ Frontend/src/app/app.config.ts
@@ -1,11 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes),
-    provideHttpClient(withFetch())
+  providers: [
+    provideRouter(routes),
+    provideHttpClient()   // 👈 أضفنا دي
   ]
 };
+
