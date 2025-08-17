e60bc4d239d8a1a4a4a4858d815d1fe2ff8f0247
sandyy44 <sandyalaa4@yahoo.com>
2025-08-17 17:10:18 +0300

    edit product service

--- Frontend/src/app/services/products.service.ts
+++ Frontend/src/app/services/products.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IProduct } from '../Models/iproduct.js';
+import { IProductRes, IProductsRes } from '../Models/iproductRes.js';
 
 @Injectable({
   providedIn: 'root'
@@ -13,11 +14,11 @@ export class ProductsService {
 
   }
 
-  getAllProducts():Observable<IProduct[]>{
-   return this.httpClient.get<IProduct[]>('https://fullstack-ecommerce-ochre.vercel.app/product')
+  getAllProducts():Observable<IProductsRes>{
+   return this.httpClient.get<IProductsRes>('https://fullstack-ecommerce-ochre.vercel.app/product')
   }
-  getProductById(id:number):Observable<IProduct>{
-   return this.httpClient.get<IProduct>(`https://fullstack-ecommerce-ochre.vercel.app/product/${id}`)
+  getProductById(id:string):Observable<IProductRes>{
+   return this.httpClient.get<IProductRes>(`https://fullstack-ecommerce-ochre.vercel.app/product/${id}`)
   }
  
 }
